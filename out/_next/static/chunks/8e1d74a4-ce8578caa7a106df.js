ca6ce6b80f1c6d466abf836723e1f50401ba70bd
Rui Fernandes <rpaf.fernandes@gmail.com>
2025-05-05 11:11:15 +0100

    small changes

--- out/_next/static/chunks/8e1d74a4-ce8578caa7a106df.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[711],{9911:(t,a,e)=>{e.d(a,{liQ:()=>c});var l=e(4436);function c(t){return(0,l.k5)({tag:"svg",attr:{viewBox:"0 0 320 512"},child:[{tag:"path",attr:{d:"M311.9 260.8L160 353.6 8 260.8 160 0l151.9 260.8zM160 383.4L8 290.6 160 512l152-221.4-152 92.8z"},child:[]}]})(t)}}}]);
