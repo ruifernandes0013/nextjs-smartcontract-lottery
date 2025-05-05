c150f96d01682b9c448b2ad04e1c5db4d83be565
Rui Fernandes <rpaf.fernandes@gmail.com>
2025-05-05 09:11:19 +0100

    make the routes compatible with the Edge Runtime

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import Raffle from "@/components/Raffle";
 
+export const runtime = "edge";
+
 export default function Home() {
   return (
     <div className="flex items-center justify-center">
